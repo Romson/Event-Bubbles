4f0cabc9b0fbb3fd15af511a2017e5da816b14ac
Romero Lijkwan <romerolijkwan@gmail.com>
2018-06-08 20:07:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // The event bubbles up from parent to parent until it is handled, or until it reaches the document object.
 // https://www.w3schools.com/jsref/event_bubbles.asp
 
-
 var sections = document.getElementsByTagName('section');
 
 for (var index = 0; index < sections.length; ++index) {
